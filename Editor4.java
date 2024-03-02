361b08532991491b31c04d9dc3c15f987c2b2c70
YuvGev <yuval.geva01@post.runi.ac.il>
2024-03-02 13:17:11 +0200

    HW6

--- Editor4.java
+++ Editor4.java
@@ -1,22 +1,26 @@
+
 import java.awt.Color;
 
 /**
- * Demonstrates the morphing operation featured by Runigram.java. 
- * The program recieves two command-line arguments: a string representing the name
- * of the PPM file of a source image, and the number of morphing steps (an int).
- * The editor will morph the given image to its grey scale version in the given
- * number of steps. 
- * 
- **/
-
+ * Demonstrates the morphing operation featured by Runigram.java.
+ * The program recieves three command-line arguments: a string representing the
+ * name
+ * of the PPM file of a source image, a string representing the name of the PPM
+ * file
+ * of a target image, and the number of morphing steps (an int).
+ * For example, to morph the cake into ironman in 50 steps, use:
+ * java Editor3 cake.ppm ironman.ppm 50
+ * Note: There is no need to scale the target image to the size of the source
+ * image, since Runigram.morph performs this action.
+ */
 public class Editor4 {
-    
-    public static void main (String[] args) {
-		String source = args[0];
-		int n = Integer.parseInt(args[1]);
-		Color[][] image = Runigram.read(source);
-        Color[][] gsImage = Runigram.grayScaled(image);
-		Runigram.setCanvas(image);
-		Runigram.morph(image, gsImage, n);
-	}
+
+    public static void main(String[] args) {
+        String source = args[0];
+        int n = Integer.parseInt(args[1]);
+        Color[][] sourceImage = Runigram.read(source);
+        Color[][] targetImage = Runigram.grayScaled(sourceImage);
+        Runigram.setCanvas(sourceImage);
+        Runigram.morph(sourceImage, targetImage, n);
+    }
 }
